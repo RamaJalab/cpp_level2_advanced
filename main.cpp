ba4f8a72f2471d5865a14d3f12496e308869ccfc
ramajalab63 <ramajalab63@gmail.com>
2026-01-17 03:27:22 +0300

    Practice debugging arrays with breakpoints.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 using namespace std;
+void readfun(int a[10])
+{
+  int i;
+  for ( i = 0; i < 10; i++)
+  {
+cout<<"Value of a  " <<i+1<<endl;
+cin>>a[i];
+    /* code */
+  }
+}
+void writefun(int a[10])
+{
+  int i;
+  for (i = 0; i< 10; i++)
+  {
+
+cout << "value of a  " <<i+1<< " = " << a[i]<<endl;
+    /* code */  
+  }
+}
 int main()
 {
-
-
-    cout << "hello";
+int a[10];
+readfun(a);
+writefun(a);
+   
     /* code */
     return 0;
 }
