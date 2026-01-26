50ba8afb74673bc8635749e32bf857eeb88ef22f
ramajalab63 <ramajalab63@gmail.com>
2026-01-27 02:20:12 +0300

    feat: connect main.cpp to new fun.h function.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include "fun.h"
 // void readfun(int a[2])
 // {
 //   int i;
@@ -28,32 +29,33 @@ using namespace std;
 // int decrease(int &m)
 // {
 //   return (m--);
+// // }
+// int MySum(int a, int b)
+// {
+// int s = 0;
+// s = a + b;
+// return s;
 // }
-int MySum(int a, int b)
-{
-int s = 0;
-s = a + b;
-return s;
-}
 int main()
 {
-  int arr1[5] = { 200,100,50,25,30 };
-int a, b, c;
-a = 10;
-b = 20;
-a++;
-++b;
-c = a + b;
-cout << a << endl;
-cout << b << endl;
-cout << c << endl;
-for (int i = 1; i <= 5; i++)
-{
-cout << i << endl;
-a = a + a * i;
-}
-c = MySum(a, b);
-cout << c;
+  mylib :: test();
+//   int arr1[5] = { 200,100,50,25,30 };
+// int a, b, c;
+// a = 10;
+// b = 20;
+// a++;
+// ++b;
+// c = a + b;
+// cout << a << endl;
+// cout << b << endl;
+// cout << c << endl;
+// for (int i = 1; i <= 5; i++)
+// {
+// cout << i << endl;
+// a = a + a * i;
+// }
+// c = MySum(a, b);
+// cout << c;
 //   int s=10;
 //   int m=4;
 //   int a[2];
